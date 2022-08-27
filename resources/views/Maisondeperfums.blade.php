1b024cd43bee7d85d7b56cb331fbadb11191e596
Narensingh85 <111748888+Narensingh85@users.noreply.github.com>
2022-08-27 15:14:05 +0530

    Update Maisondeperfums.blade.php

--- resources/views/Maisondeperfums.blade.php
+++ resources/views/Maisondeperfums.blade.php
@@ -121,7 +121,7 @@
 <div class="container mt-5">
   <div class="row">
     <div class="col-md-4">
-      <img src="pictures/maisonperfumes.svg" width="100%" alt="" />
+      <img src="pictures/maisonperfumes.svg" width="70%" alt="" />
     </div>
     <div class="col-md-8 mt-5">
       <p style="text-align: justify;">
